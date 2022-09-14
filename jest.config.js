56ded0262f8180a260fabc97001a524b10d8e493
Brian Swedberg <bwswedberg@gmail.com>
2022-09-14 05:28:18 -0400

    Added coverage config.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
