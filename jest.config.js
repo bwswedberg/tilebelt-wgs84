a73c6cdaced60ef4fbc25726d4454ef086ed6470
Brian Swedberg <bwswedberg@gmail.com>
2022-10-12 08:51:11 -0400

    Updated config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
   preset: 'ts-jest',
+  resolver: 'ts-jest-resolver',
   testEnvironment: 'node',
   rootDir: '.',
-  extensionsToTreatAsEsm: ['.ts'],
   coverageThreshold: {
     global: {
       branches: 100,
